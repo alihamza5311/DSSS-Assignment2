2e999c18b71c621f841270cae3f9172261069727
Ali Hamza DSSS Homework 2 <engralihamza2@gmail.com>
2023-11-11 04:08:58 +0100

    Commit

--- math_quiz.py
+++ math_quiz.py
@@ -1,42 +1,51 @@
-import random
+from random import randint, choice
 
-
-def function_A(min, max):
+def function_Integer(min, max):
     """
     Random integer.
     """
-    return random.randint(min, max)
+    return randint(min, max)
 
+# Random number Generator for function A
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def function_Operator():
+    return choice(['+', '-', '*'])
 
+# Random Math operation selection for function B
 
-def function_C(n1, n2, o):
+def function_Execution(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+':
+        a = n1 + n2
+    elif o == '-':
+        a = n1 - n2
+    else:
+        a = n1 * n2
     return p, a
+
+# Operation performing
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
+# Quiz result storing parameters
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = function_Integer(1, 10)
+        n2 = function_Integer(1, 5)
+        o = function_Operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = function_Execution(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
